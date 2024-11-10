9d0b831c09db1b2831fbec1eea156c98a03a76c4
soteliin <soteliin5@gmail.com>
2024-11-10 12:16:37 -0600

    find by id para cotizaciones de usuario

--- server.js
+++ server.js
@@ -79,6 +79,76 @@ app.post('/login', upload.none(), async (req, res) => {
     }
 });
 
+app.post('/add-cotizacion', upload.none(), async (req, res) => {
+    console.log('POST /add-cotizacion');
+
+    try {
+        const {
+            id_casa,
+            id_tipo_prestamo,
+            id_amortizacion,
+            id_plazo,
+            correo_cliente,
+        } = req.body;
+
+        if (!id_casa || !id_tipo_prestamo || !id_amortizacion || !id_plazo || !correo_cliente) 
+            return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+
+        const result = await pool.query(
+            `INSERT INTO segundop.tr_cotizacion (
+                id_casa, id_tipo_prestamo, id_amortizacion, id_plazo, correo_cliente
+            ) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [id_casa, id_tipo_prestamo, id_amortizacion, id_plazo, correo_cliente]
+        );
+        res.status(201).json({
+            message: 'Cotización añadida exitosamente',
+            cotizacion: result.rows[0],
+        });
+    } catch (err) {
+        console.error('Error inserting cotización:', err);
+        res.status(500).json({ error: 'Error al añadir la cotización' });
+    }
+});
+
+app.put('/edit-profile', async (req, res) => {
+    console.log('PUT /edit-profile');
+
+    const {
+        nombre_completo,
+        rfc,
+        edad,
+        telefono,
+        correo,
+        sueldo,
+        id_estado_civil
+    } = req.body;
+
+    try {
+        const result = await pool.query(
+            `UPDATE segundop.tr_cliente
+         SET 
+           nombre_completo = COALESCE($1, nombre_completo),
+           rfc = COALESCE($2, rfc),
+           edad = COALESCE($3, edad),
+           telefono = COALESCE($4, telefono),
+           sueldo = COALESCE($5, sueldo),
+           id_estado_civil = COALESCE($6, id_estado_civil)
+         WHERE correo = $7
+         RETURNING *`,
+            [nombre_completo, rfc, edad, telefono, sueldo, id_estado_civil, correo]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User updated successfully', user: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error updating the user' });
+    }
+});
+
 app.get('/estado-civil', async (req, res) => {
     console.log('GET /estado-civil');
     try {
@@ -174,46 +244,6 @@ app.get('/get-user-sueldo', async (req, res) => {
     }
 });
 
-
-app.put('/edit-profile', async (req, res) => {
-    console.log('PUT /edit-profile');
-
-    const {
-        nombre_completo,
-        rfc,
-        edad,
-        telefono,
-        correo,
-        sueldo,
-        id_estado_civil
-    } = req.body;
-
-    try {
-        const result = await pool.query(
-            `UPDATE segundop.tr_cliente
-         SET 
-           nombre_completo = COALESCE($1, nombre_completo),
-           rfc = COALESCE($2, rfc),
-           edad = COALESCE($3, edad),
-           telefono = COALESCE($4, telefono),
-           sueldo = COALESCE($5, sueldo),
-           id_estado_civil = COALESCE($6, id_estado_civil)
-         WHERE correo = $7
-         RETURNING *`,
-            [nombre_completo, rfc, edad, telefono, sueldo, id_estado_civil, correo]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        res.status(200).json({ message: 'User updated successfully', user: result.rows[0] });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error updating the user' });
-    }
-});
-
 app.get('/get-house', async (req, res) => {
     console.log('GET /get-house');
 
@@ -227,36 +257,55 @@ app.get('/get-house', async (req, res) => {
     }
 });
 
-app.post('/add-cotizacion', upload.none(), async (req, res) => {
-    console.log('POST /add-cotizacion');
+app.get('/get-prestamo-byid', async (req, res) => {
+    console.log('GET /get-prestamo-byid');
 
+    const { id_tipo_prestamo } = req.query;
     try {
-        const {
-            id_casa,
-            id_tipo_prestamo,
-            id_amortizacion,
-            id_plazo,
-            correo_cliente,
-        } = req.body;
-
-        if (!id_casa || !id_tipo_prestamo || !id_amortizacion || !id_plazo || !correo_cliente) {
-            return res.status(400).json({ error: 'Todos los campos son obligatorios' });
-        }
-
-        const result = await pool.query(
-            `INSERT INTO segundop.tr_cotizacion (
-                id_casa, id_tipo_prestamo, id_amortizacion, id_plazo, correo_cliente
-            ) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-            [id_casa, id_tipo_prestamo, id_amortizacion, id_plazo, correo_cliente]
-        );
-
-        res.status(201).json({
-            message: 'Cotización añadida exitosamente',
-            cotizacion: result.rows[0],
-        });
+        const result = await pool.query('SELECT * FROM segundop.tc_tipo_prestamo WHERE id_tipo_prestamo = $1', [id_tipo_prestamo]);
+        res.status(200).json(result.rows);
     } catch (err) {
-        console.error('Error inserting cotización:', err);
-        res.status(500).json({ error: 'Error al añadir la cotización' });
+        console.error(err);
+        res.status(500).json({ error: 'Error al obtener los datos del prestamo' });
+    }
+});
+
+app.get('/get-amortizacion-byid', async (req, res) => {
+    console.log('GET /get-amortizacion-byid');
+
+    const { id_amortizacion } = req.query;
+    try {
+        const result = await pool.query('SELECT prestamista FROM segundop.tc_amortizacion WHERE id_amortizacion = $1', [id_amortizacion]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al obtener los datos de la amortizacion' });
+    }
+});
+
+app.get('/get-plazo-byid', async (req, res) => {
+    console.log('GET /get-plazo-byid');
+
+    const { id_plazo } = req.query;
+    try {
+        const result = await pool.query('SELECT plazo FROM segundop.tc_plazo WHERE id_plazo = $1', [id_plazo]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al obtener los datos del plazo' });
+    }
+});
+
+app.get('/get-coti-usr', async (req, res) => {
+    console.log('GET /get-coti-usr');
+
+    const { email } = req.query;
+    try {
+        const result = await pool.query('SELECT * FROM segundop.tr_cotizacion WHERE correo_cliente = $1', [email]);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al obtener las cotizaciones del usuario' });
     }
 });
 
